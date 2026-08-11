c97c809e87e4f3f5067eddb463fd2ec20795a067
moodibo1 <ibraheemziyad45@gmail.com>
2026-08-11 11:49:52 +0300

    chore: prepare for Render deployment (CORS, requirements.txt fix)

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Factory Issues API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
